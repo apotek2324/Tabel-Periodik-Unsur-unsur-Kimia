959fa1f79d0a410d8ef24a3cd9981e9c9b4acd9b
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-04-26 18:09:35 +0700

    Update ph_kalkulator.py

--- ph_kalkulator.py
+++ ph_kalkulator.py
@@ -1,13 +1,14 @@
+import streamlit as st
 senyawa_asam_kuat = st.selectbox(
     "Masukkan senyawa asam",
     ("Asam Nitrat-HNO3","Asam Klorida-HCl","Asam Sulfat-H2SO4","Asam Iodida-HI","Asam Flourida-HF"),
-index=None,
-   placeholder="Pilih senyawa...",)
+    index=None,
+    placeholder="Pilih senyawa...",)
+st.write('Anda memilih:', option)
 
 senyawa_basa_kuat = st.selectbox(
     "Masukkan senyawa basa"
     ("Kalium Hidroksida-KOH","Natrium Hidroksida-NaOH","Litium Hidroksida-LiOH","Kalsium Hidroksida-Ca(OH)2"),
-index=None,
-   placeholder="Pilih senyawa...",)
-
+    index=None,
+    placeholder="Pilih senyawa...",)
 st.write('Anda memilih:', option)
