4602b0411841593f8e4bbec8095388ee65186028
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-04-27 13:35:46 +0700

    Update ph_kalkulator.py

--- ph_kalkulator.py
+++ ph_kalkulator.py
@@ -3,20 +3,15 @@ from streamlit_option_menu import option_menu
 
 # navigasi sidebar
 with st.sidebar:
-    selected = option_menu("Perhitungan pH Larutan", ['Menghitung dengan Konsentrasi', 'Menghitung dengan Massa dan Volume'], 
-    default_index=1)
-    selected
+    selected = option_menu(
+        menu_title = "Perhitungan pH Larutan", #required 
+        options =["Menghitung dengan Konsentrasi", "Menghitung dengan Massa dan Volume"], #required 
+    )
 
-# halaman perhitungan pH
-if (selected == 'Menghitung dengan Konsentrasi') :
-    st.title('Kalkulator pH Larutan')
-    
-    senyawa_asam_kuat = st.selectbox(
-    "Masukkan senyawa",
-    ("Asam Nitrat-HNO3","Asam Klorida-HCl","Asam Sulfat-H2SO4","Asam Iodida-HI","Asam Flourida-HF"),
-    index=None,
-    placeholder="Pilih senyawa...",)
-    st.write('Anda memilih:', senyawa_asam_kuat)
+if (selected == "Menghitung dengan Konsentrasi") :
+    st.title(f"Kalkulator pH Larutan {selected}")
+if (selected == "Menghitung dengan Massa dan Volume") :
+    st.title(f"Kalkulator Larutan {selected}")
 
 
     
