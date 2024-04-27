871ca73d905754a45bf634a7a3189887a945c652
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-04-27 12:59:47 +0700

    Update ph_kalkulator.py

--- ph_kalkulator.py
+++ ph_kalkulator.py
@@ -1,20 +1,22 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
-st.title('Kalkulator pH Larutan')
+# navigasi sidebar
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Menghitung dengan Konsentrasi', 'Menghitung dengan Massa dan Volume'], 
+    default_index=0)
 
-your-repository/
-├── pages/
-│   ├── Asam_Kuat_dan_Basa_Kuat.py
-│   ├── Asam_Kuat_dan_Basa_Lemah.py
-│   ├── Asam_Lemah_dan_Basa_Kuat.py
-│   └── Asam_Lemah_dan_Basa_Lemah.py
-└── Kalkulator_pH_Campuran.py
-
-st.page_link("Kalkulator_pH_Campuran.py", label="Kalkulator pH", icon="🏠")
-st.page_link("Asam_Kuat_dan_Basa_Kuat.py", label="Page 1", icon="1️⃣")
-st.page_link("Asam_Kuat_dan_Basa_Lemah.py", label="Page 2", icon="2️⃣")
-st.page_link("Asam_Lemah_dan_Basa_Kuat.py", label="Page 3", icon="🌎")
-st.page_link("Asam_Lemah_dan_Basa_Lemah.py", label="Page 4", icon="p", disabled=True)
+# halaman perhitungan pH
+if (selected == 'Menghitung dengan Konsentrasi') :
+    st.title('Kalkulator pH Larutan')
     
+    senyawa_asam_kuat = st.selectbox(
+    "Masukkan senyawa",
+    ("Asam Nitrat-HNO3","Asam Klorida-HCl","Asam Sulfat-H2SO4","Asam Iodida-HI","Asam Flourida-HF"),
+    index=None,
+    placeholder="Pilih senyawa...",)
+    st.write('Anda memilih:', senyawa_asam_kuat)
+
+
     
 
