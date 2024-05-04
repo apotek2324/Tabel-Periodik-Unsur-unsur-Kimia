e9078b78cdc0c1e6ac8430f8170c4e9bc9e4ce7c
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-05-05 06:21:08 +0700

    Update ph_kalkulator.py

--- ph_kalkulator.py
+++ ph_kalkulator.py
@@ -11,9 +11,7 @@ if hitung:
     asam = konsentrasi * valensi
     st.write('Hasil = ', asam)
     st.success(f'Hasil = {asam}')
-    pH = log10(asam)
-    st.write('Hasil = ', pH)
-print('log (fabs(x), base) is :',math.log(math.fabs(asam),10))
+    print('log (fabs(x), base) is :',math.log(math.fabs(asam),10))
 
 
 
