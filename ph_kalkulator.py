b7769bac7ee468f66b41c1b8092491427afe73e1
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-05-05 06:35:19 +0700

    Update ph_kalkulator.py

--- ph_kalkulator.py
+++ ph_kalkulator.py
@@ -5,7 +5,7 @@ st.title('Kalkulator pH Larutan')
 
 konsentrasi = st.number_input('Masukkan Konsentrasi')
 st.write("Konsentrasi = ", konsentrasi)
-valensi = st.number_input('Masukkan Valensi')
+valensi = st.number_input('Masukkan Valensi', 0)
 st.write("Valensi = ", valensi)
 hitung = st.button('Hitung pH')
 
