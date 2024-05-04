34ef894f33a0c4651a6d8346fdad13b1d37c1bf1
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-05-04 21:58:39 +0700

    Update ph_kalkulator.py

--- ph_kalkulator.py
+++ ph_kalkulator.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.title('Kalkulator pH Larutan')
 
-konsentrasi = st.text_input('Masukkan Konsentrasi', 0)
-valensi = st.text_input('Masukkan Valensi', 0)
+konsentrasi = st.number_input('Masukkan Konsentrasi', 0)
+valensi = st.number_input('Masukkan Valensi', 0)
 hitung = st.button('Hitung pH')
 
 if hitung:
