6c8e6e2471ec4052a1af5a7d9775ff7e29cb4279
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-05-05 06:27:57 +0700

    Update ph_kalkulator.py

--- ph_kalkulator.py
+++ ph_kalkulator.py
@@ -3,15 +3,15 @@ import math
 
 st.title('Kalkulator pH Larutan')
 
-konsentrasi = st.number_input('Masukkan Konsentrasi', 0)
-valensi = st.number_input('Masukkan Valensi', 0)
+konsentrasi = st.text_input('Masukkan Konsentrasi', 0)
+valensi = st.text_input('Masukkan Valensi', 0)
 hitung = st.button('Hitung pH')
 
 if hitung:
     asam = konsentrasi * valensi
     st.write('Hasil = ', asam)
     st.success(f'Hasil = {asam}')
-    st.write('log(fabs(x), base, is', math.log(math.fabs(asam),10))
+    st.write('log(fabs(x), base) is', math.log(math.fabs(asam),10))
     st.write('Hasil = ', pH)
 
 
