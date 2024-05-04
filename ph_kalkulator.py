14f47d86dbcddffc5a4e8dcb13a9ab73fe7aa168
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-05-04 21:35:01 +0700

    Update ph_kalkulator.py

--- ph_kalkulator.py
+++ ph_kalkulator.py
@@ -4,6 +4,10 @@ st.title('Kalkulator pH Larutan')
 
 Masukkan_Konsentrasi = st.text_input('Masukkan Konsentrasi')
 Masukkan_Valensi = st.text_input('Masukkan Valensi')
+hitung = st.button('Hitung pH')
+
+if hitung:
+    H = konsentrasi * valensi
 
 
 
